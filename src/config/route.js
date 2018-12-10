cd3756db2045a59766d01c5af4374d21073806b6
Yhspehy <670395851@qq.com>
2018-12-10 13:48:36 +0800

    add: init

---
+++ src/config/route.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import { createAppContainer } from 'react-navigation';
+
+import TabNavigator from '../view/BarPage/index'
+
+export default createAppContainer(TabNavigator);
